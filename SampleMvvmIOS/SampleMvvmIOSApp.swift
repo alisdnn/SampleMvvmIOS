cbd2df51c44570897331df6f581625090ec35a1d
Ali Sadeghian <aliosdn@gmail.com>
2024-08-11 22:59:44 -0400

    Initial Commit

---
+++ SampleMvvmIOS/SampleMvvmIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleMvvmIOSApp.swift
+//  SampleMvvmIOS
+//
+//  Created by ali sadeghian on 2024-08-11.
+//
+
+import SwiftUI
+
+@main
+struct SampleMvvmIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
